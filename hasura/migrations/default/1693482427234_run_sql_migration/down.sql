8ba15f82dd16208876f476dfbed33470b6516c12
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-08-31 14:47:26 +0300

    CREATE TYPE recommend_return_type

---
+++ hasura/migrations/default/1693482427234_run_sql_migration/down.sql
@@ -0,0 +1,6 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TYPE recommend_return_type AS (
+--   product_id INT,
+--   recommendation_score NUMERIC
+-- );
---
+++ hasura/migrations/default/1693482427234_run_sql_migration/up.sql
@@ -0,0 +1,4 @@
+CREATE TYPE recommend_return_type AS (
+  product_id INT,
+  recommendation_score NUMERIC
+);
